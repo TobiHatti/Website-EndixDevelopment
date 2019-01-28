302aa678994dacf3ad5f66bafb23d53de6f4f71d
TobiHatti <32573581+TobiHatti@users.noreply.github.com>
2019-01-28 17:10:29 +0100

    Created animation for first load

--- _footer.php
+++ _footer.php
@@ -1,3 +1,12 @@
 <?php
 
+    echo '
+                </main>
+                <footer>
+
+                </footer>
+            </body>
+        </html>
+    ';
+
 ?>
--- _header.php
+++ _header.php
@@ -1,3 +1,41 @@
 <?php
 
+
+    echo '
+        <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="/css/style.css">
+            </head>
+            <body>
+                <header>
+                    <div class="startLoader">
+                        <center>
+                            <div class="startLoaderImageContainer">
+                                <div class="startLogoImage">
+                                    <img src="/content/LogoBackground.png" alt="" />
+                                    <img src="/content/***REMOVED***.png" alt="" />
+                                </div>
+                            </div>
+                            <div class="startLoaderCover"></div>
+                        </center>
+                    </div>
+
+                    <div class="headerLogoContainer">
+                        <div class="headerLogo">
+                            <img src="/content/LogoBackground.png" alt="" />
+                            <img src="/content/***REMOVED***.png" alt="" />
+                        </div>
+                        <div class="headerName">
+                            Hallo Ihr s&auml;cke
+                        </div>
+                    </div>
+                </header>
+                <nav>
+
+
+                </nav>
+                <main>
+    ';
+
+
 ?>
