717717c740e09231bb0799c30b59e8545d0657ca
TobiHatti <32573581+TobiHatti@users.noreply.github.com>
2019-01-28 17:10:29 +0100

    Created animation for first load

--- index.php
+++ index.php
@@ -1,55 +1,16 @@
-<style type="text/css">
+<?php
 
-@keyframes logoBGRotate{
-    0%{
-        transform: rotate(0deg);
-    }
-
-    100%{
-        transform: rotate(359deg);
-    }
-
-}
-
-.logoContainer{
-    position: relative;
-
-    width: 200px;
-    height: 200px;
-
-    overflow: hidden;
-}
-
-.logoContainer img:nth-child(1){
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
-
-    image-rendering: crisp-edges;
-
-    animation: logoBGRotate;
-    animation-timing-function: linear;
-    animation-duration: 30s;
-    animation-iteration-count: infinite;
-}
-
-.logoContainer img:nth-child(2){
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
-
-    image-rendering: crisp-edges;
-}
+    require("_header.php");
 
 
-</style>
 
 
-<div class="logoContainer">
-    <img src="/content/LogoBackground.png" alt="" />
-    <img src="/content/LogoCenterTransparentSmoothCutout.png" alt="" />
-</div>
+    echo 'asdasdasdasdsadsd';
+
+
+
+
+
+    include("_footer.php");
+
+?>
---
+++ loader.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+
+<style type="text/css">
+
+
+
+</style>
+
+
+<!-- Start of first-time load -->
+
+<div class="startLoader">
+    <center>
+        <div class="startLoaderImageContainer">
+            <div class="startLogoImage">
+                <img src="/content/LogoBackground.png" alt="" />
+                <img src="/content/LogoCenterTransparentSmoothCutout.png" alt="" />
+            </div>
+        </div>
+        <div class="startLoaderCover"></div>
+    </center>
+</div>
+
+<div class="headerLogoContainer">
+    <div class="headerLogo">
+        <img src="/content/LogoBackground.png" alt="" />
+        <img src="/content/LogoCenterTransparentSmoothCutout.png" alt="" />
+    </div>
+    <div class="headerName">
+        Hallo Ihr s&auml;cke
+    </div>
+</div>
+
+<!-- End of first-time load -->
+
+
+<br><br><br><br><br><br><br><br><br><br>
+<button>asdasdasdasdasd<br><br><br><br><br><br>asdasdasdasd</button>
