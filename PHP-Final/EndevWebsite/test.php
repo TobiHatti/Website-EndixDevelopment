76e89b75208f9e9be2e5d9a3eb5ed0e0b288b798
TobiHatti <tobias.hattinger@gmx.at>
2020-06-25 23:49:42 +0200

    Added index-header-BG

---
+++ PHP-Final/EndevWebsite/test.php
@@ -0,0 +1,9 @@
+<style>
+    html{
+        background: rgb(16,0,255);
+    background: -moz-linear-gradient(80deg, rgba(16,0,255,1) 0%, rgba(0,146,255,1) 8%, rgba(41,214,214,1) 45%, rgba(41,214,214,1) 55%, rgba(0,146,255,1) 92%, rgba(16,0,255,1) 100%);
+    background: -webkit-linear-gradient(80deg, rgba(16,0,255,1) 0%, rgba(0,146,255,1) 8%, rgba(41,214,214,1) 45%, rgba(41,214,214,1) 55%, rgba(0,146,255,1) 92%, rgba(16,0,255,1) 100%);
+    background: linear-gradient(80deg, rgba(16,0,255,1) 0%, rgba(0,146,255,1) 8%, rgba(41,214,214,1) 45%, rgba(41,214,214,1) 55%, rgba(0,146,255,1) 92%, rgba(16,0,255,1) 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#1000ff",endColorstr="#1000ff",GradientType=1);
+    }
+</style>
